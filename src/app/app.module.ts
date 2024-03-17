1564d3a1910e65bf1f8394909d3558307ceb93e4
Mgalazyn <m.galazynn@gmail.com>
2024-03-17 17:12:38 +0100

    header/navbar, footer, home sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IonicModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  isMenuOpen: boolean = false; // Track the state of the menu
+
+  toggleMenu(): void {
+    const list = document.querySelector('ul')!;
+    const icon = document.querySelector('.menu-icon') as HTMLElement;
+
+    // Toggle the menu state
+    this.isMenuOpen = !this.isMenuOpen;
+
+    // Apply CSS classes first
+    if (this.isMenuOpen) {
+      list.classList.add('top-[80px]', 'opacity-100');
+    } else {
+      list.classList.remove('top-[80px]', 'opacity-100');
+    }
+
+    // Change icon after applying CSS classes
+    icon.setAttribute('name', this.isMenuOpen ? 'close-outline' : 'menu-outline');
+  }
+}
