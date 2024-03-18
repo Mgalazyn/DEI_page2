56c78aaef9cc9bae3ef9d8e6d6a8f2ddd742e01d
Mgalazyn <m.galazynn@gmail.com>
2024-03-18 23:05:48 +0100

    project change

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+    "./src/app/*.{html,ts}",
+    "./src/app/components/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customYellow: '#FFCD00',
+      },
+      fontFamily: {
+        'plus-jakarta-sans': ['Plus Jakarta Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
