b2961e80bc856aeac52997a0c127dbadaf4e7e88
Mgalazyn <m.galazynn@gmail.com>
2024-03-25 22:57:26 +0100

    reponsivness of all components

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
     extend: {
       spacing: {
         '-160': '-40rem', // Adjust the value as needed
+        '-120': '-30rem',
         '-200': '-50rem',
         '-256': '-64rem',
       },
