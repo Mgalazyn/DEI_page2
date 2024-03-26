dbd3e7a7ef9de90511520b65f53d09f5bf9ffad1
Mgalazyn <m.galazynn@gmail.com>
2024-03-26 18:57:23 +0100

    AOS animations, hover effects on buttons

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import AOS from "aos";
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {
-
+export class AboutComponent implements OnInit {
+  constructor() {}
+    ngOnInit(): void {
+        AOS.init();
+    }
 }
